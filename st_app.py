1b838161e5af85b59ed5904a2c4c7df014a02faf
NEETU95 <neetu0708@gmail.com>
2024-01-30 18:58:01 +0530

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -8,13 +8,13 @@ def st_main():
 	print('params : ',params)
 	pdf_info = params.get("pdf_info", [""])
 	print("pdf_info : ", str(pdf_info))
-
-	try:
-		trail = pdf_extraction(pdf_info)
-		return trail
-	except Exception as e:
-		print("error from app", e)
-		return 
+	if pdf_info:
+		try:
+			trail = pdf_extraction(pdf_info)
+			return trail
+		except Exception as e:
+			print("error from app", e)
+			return 
 
 if __name__ == "__main__":
     st_main()
